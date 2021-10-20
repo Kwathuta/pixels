3d8fb35086fbc72ef5f24b0c7d258598268eef36
Kwathuta <kelvin.gitahi@student.moringaschool.com>
2021-10-20 17:43:41 +0300

    finalizing design

--- pixels_app/views.py
+++ pixels_app/views.py
@@ -167,6 +167,6 @@ def save_image(request):
         image = Image(name=image_name, caption=image_caption, image=image_file,
                       profile_id=request.POST['user_id'], user_id=request.POST['user_id'])
         image.save_image()
-        return redirect('/profile', {'success': 'Image Uploaded Successfully'})
+        return redirect('/', {'success': 'Image Uploaded Successfully'})
     else:
         return render(request, 'profile.html', {'danger': 'Image Upload Failed'})
